a2ce99adfdc85732d271f30698f8c14e106df836
Serge Chvorun <elifnull@protonmail.com>
2022-02-21 20:16:21 -0500

    personcard update

---
+++ person_card/src/components/PersonCard.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const PersonCard = (props) => {
+    const {firstName, lastName, age, hairColour} = props;
+    return(
+        <div className="personCard">
+            <ul>
+            <li> {lastName}, {firstName} </li>
+            <li> Age: {age}</li>
+            <li> Hair Colour: {hairColour}</li>
+            </ul>
+        </div>
+    )
+};
+
+export default PersonCard;
