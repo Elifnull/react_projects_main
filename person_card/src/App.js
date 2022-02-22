5df5dc4f0991eee3e795f6c35943ee9742fbfd10
Serge Chvorun <elifnull@protonmail.com>
2022-02-21 20:15:52 -0500

    props update

--- person_card/src/App.js
+++ person_card/src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import PersonCard from '../../jsx/first_jsx/src/components/Person';
+import PersonCard from './components/PersonCard';
 
 function App() {
   return (
     <div className="App">
-      <PersonCard firstName = />
-      
+      <PersonCard firstName = {"Serge"} lastName= {"Chvorun"} age= {33} hairColour= {"Black"}/>
+      <PersonCard firstName = {"Billy"} lastName= {"valdin"} age= {23} hairColour= {"Blue"}/>
+      <PersonCard firstName = {"Ephriam"} lastName= {"Chvorun"} age= {10} hairColour= {"Blond"}/>
+      <PersonCard firstName = {"Elisha"} lastName= {"Chvorun"} age= {19} hairColour= {"Brown"}/>
     </div>
   );
 }
