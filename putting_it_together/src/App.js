26a2854ab06b3a2eccbf778ec880b0388e4df692
Serge Chvorun <elifnull@protonmail.com>
2022-02-25 23:15:31 -0500

    form & verification

--- putting_it_together/src/App.js
+++ putting_it_together/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Personcard from './components/Personcard';
+import Form from './components/Form';
 
 function App() {
   return (
     <div className="App">
       <Personcard firstName="Serge" lastName="Chvorun" Age = {34} hairColour="Black"/>
       <Personcard firstName="Ephraim" lastName="Chvorun" Age = {12} hairColour="Blond"/>
+      <br/>
+      <br/>
+      <Form/>
     </div>
   );
 };
---
+++ putting_it_together/src/components/Form.js
@@ -0,0 +1,56 @@
+import React, {useState} from "react";
+import "../Form.css";
+
+const Form = () => {
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email,setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [passConfirm, setPassConfirm] = useState('');
+
+    const createUser = (e) => {
+
+        e.preventDefault();
+        const newUser = {firstName, lastName, email, password};
+        setFirstName('');
+        setLastName('');
+        setEmail('');
+        setPassword('');
+    };
+    return(
+        <div>
+            <form onSubmit={createUser}>
+                <div>
+                    <label>First Name:</label>
+                    <input type="text" value={firstName} onChange={ (e) => setFirstName(e.target.value) } />
+                </div>
+                <div>
+                    <label>Last Name:</label>
+                    <input type="text" value={lastName} onChange={ (e) => setLastName(e.target.value) } />
+                </div>
+                <div>
+                    <label>Email:</label>
+                    <input type="email" value={email} onChange={ (e) => setEmail(e.target.value) } />
+                </div>
+                <div>
+                    <label>Password:</label>
+                    <input type="password" value={password} onChange={ (e) => setPassword(e.target.value) } />
+                </div>
+                <div>
+                    <label>Confirm Password:</label>
+                    <input type="password" value={passConfirm} onChange={ (e) => setPassConfirm(e.target.value) } />
+                </div>
+            </form>
+            <list>
+                <p>Form Data</p>
+                <p>First Name: {firstName}</p>
+                <p>Last Name: {lastName}</p>
+                <p>Email: {email}</p>
+                <p>Password: {password}</p>
+                <p>Password Confirm: {passConfirm}</p>
+            </list>
+        </div>
+    );
+};
+
+export default Form;
