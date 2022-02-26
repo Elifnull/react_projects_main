149be05fabe1da331395600bee05ea2a137b34e6
Serge Chvorun <elifnull@protonmail.com>
2022-02-25 21:31:18 -0500

    two way data binding update

--- important_details/forms.js
+++ important_details/forms.js
@@ -8,29 +8,28 @@ const UserForm = (props) => {
     const createUser = (e) => {
         // we must prevent the default refresh of the browser to keep our state from being reset
         e.preventDefault();
-    
-        // create a javascript object to hold all of the values
-        const newUser = { 
-            username: username, 
-            email: email, 
-            password: password 
-        };
+        
+        // shorthand ES6 syntax for building an object - see notes above
+        const newUser = { username, email, password };
         console.log("Welcome", newUser);
+    	setUsername("");
+    	setEmail("");
+    	setPassword("");
     };
     
     return(
         <form onSubmit={ createUser }>
             <div>
-                <label>Username: </label> 
-                <input type="text" onChange={ (e) => setUsername(e.target.value) } />
+                <label>Username: <label> 
+                <input type="text" value={username} onChange={ (e) => setUsername(e.target.value) } />
             </div>
             <div>
                 <label>Email Address: </label> 
-                <input type="text" onChange={ (e) => setEmail(e.target.value) } />
+                <input type="text" value={email} onChange={ (e) => setEmail(e.target.value) } />
             </div>
             <div>
                 <label>Password: </label>
-                <input type="text" onChange={ (e) => setPassword(e.target.value) } />
+                <input type="text" value={password} onChange={ (e) => setPassword(e.target.value) } />
             </div>
             <input type="submit" value="Create User" />
         </form>
