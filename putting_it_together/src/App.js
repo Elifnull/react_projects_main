16cb2de43781bc1d54ce8018d8a96dbca2e59df7
Serge Chvorun <elifnull@protonmail.com>
2022-02-26 23:33:13 -0500

    form validation

--- putting_it_together/src/App.js
+++ putting_it_together/src/App.js
@@ -4,9 +4,9 @@ import Form from './components/Form';
 
 function App() {
   return (
-    <div className="App">
-      <Personcard firstName="Serge" lastName="Chvorun" Age = {34} hairColour="Black"/>
-      <Personcard firstName="Ephraim" lastName="Chvorun" Age = {12} hairColour="Blond"/>
+    <div className="App"> {
+      /* <Personcard firstName="Serge" lastName="Chvorun" Age = {34} hairColour="Black"/>
+      <Personcard firstName="Ephraim" lastName="Chvorun" Age = {12} hairColour="Blond"/> */}
       <br/>
       <br/>
       <Form/>
