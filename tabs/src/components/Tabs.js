3dba738f0d64898fa78d2503948c7841e91240e6
Serge Chvorun <elifnull@protonmail.com>
2022-03-08 17:08:19 -0500

    tailwind CSS update

--- tabs/src/components/Tabs.js
+++ tabs/src/components/Tabs.js
@@ -2,14 +2,23 @@ import React from "react";
 
 const Tabs = (props) => {
     const {tabArray, activeTab, setActiveTab } = props;
-
+    const tabFunction = (tabIndex) => {
+        if (tabIndex === activeTab){
+            return "activeTab"
+        }
+    };
 
 
 
     return(
         <div>
             {tabArray.map((value, index) =>(
-            <p>{value.name}</p> 
+            <p style={{
+                display: "inline-block",
+                margin: "4px",
+                border: "2px black solid",
+                padding: "4px"
+            }} className={tabFunction(index)} key = {index} onClick= {()=>setActiveTab(index)}>{value.name}</p> 
             ))}
         </div>
     );
