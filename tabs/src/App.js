10a52bd41bac091958269f5d556d8634973bc7ef
Serge Chvorun <elifnull@protonmail.com>
2022-03-08 13:45:01 -0500

    dictionary & useState update

--- tabs/src/App.js
+++ tabs/src/App.js
@@ -1,8 +1,17 @@
 import Tabs from "./components/Tabs";
 import Display from "./components/Display";
+import { useState} from "react";
 import './App.css';
 
 function App() {
+  const {tab1, tab2, tab3} = tablist([
+    {name: "Tab 1", content: "This is Tab 1", colour: "Black"},
+    {name: "Tab 2", content: "This is Tab 2", colour: "Black"},
+    {name: "Tab 3", content: "This is Tab 3", colour: "Black"}
+  ]);
+  const [activeTab, setActiveTab] = useState(0);
+
+  
   return (
     <div className="App">
       <Tabs/>
