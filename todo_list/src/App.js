e2f7329ca61c30b7e3e1308b6a1c69b4bfe02647
Serge Chvorun <elifnull@protonmail.com>
2022-03-09 16:59:35 -0500

    filter function addition, material component removal

--- todo_list/src/App.js
+++ todo_list/src/App.js
@@ -1,11 +1,14 @@
-import Todo from "./components/Todo"
+import Todo from "./components/Todo";
+import TodoList from "./components/TodoList";
 import { useState} from "react";
 import './App.css';
 
 function App() {
   const [todoList, setTodoList] = useState([]);
+
   return (
     <div className="App">
+      <TodoList todoList = {todoList} setTodoList = {setTodoList}/>
       <Todo todoList = {todoList} setTodoList ={setTodoList} />
     </div>
   );
