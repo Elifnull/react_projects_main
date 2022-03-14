37b841a9919838ebbc4147650950dd0e55769091
Serge Chvorun <elifnull@protonmail.com>
2022-03-14 01:34:16 -0400

    final routing practice

--- route_practice/src/App.js
+++ route_practice/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link} from "react-router-dom";
+import Home from "./components/Home"
+import Display from "./components/Display";
+import DoubleDisplay from "./components/Double_display";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/home" element={<Home/>}/>
+          <Route exact path="/:value" element={<Display/>}/>
+          <Route path="/:word/:colour1/:colour2" element={<DoubleDisplay/>}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
